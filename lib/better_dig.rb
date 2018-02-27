00da701101eeea99545572befea607f4554a70ba
Bernie Chiu <bernie_chiu@hotmail.com>
2018-02-27 21:59:50 +0800

    Core module for better dig

--- lib/better_dig.rb
+++ lib/better_dig.rb
@@ -1,5 +1,16 @@
 require "better_dig/version"
 
 module BetterDig
-  # Your code goes here...
+  DIGGABLE_CLASSES = [Hash, Array].freeze
+  INDIFFERENT_FIND = { Symbol => :to_s, String => :to_sym }.freeze
+
+  def digg(key, *rest)
+    value = self.is_a?(Array) ? self[key.to_i] : self[key]
+    value = self[key.send(INDIFFERENT_FIND[key.class])] unless value
+    return value             if value.nil? || rest.empty?
+    return value.digg(*rest) if value.respond_to?(:digg)
+    nil
+  end
 end
+
+BetterDig::DIGGABLE_CLASSES.each { |klass| klass.send(:include, BetterDig) unless klass.method_defined?(:digg) }
